1d6f23f7ac711d914f55e81e514d4c49ac7c0868
devgreact <dev.greact@gmail.com>
2024-02-15 20:47:45 +0900

    Redux 기본 내용

--- src/router/root.js
+++ src/router/root.js
@@ -7,6 +7,7 @@ import { Suspense, lazy } from "react";
 import todoRouter from "./todoRouter";
 import productRouter from "./productRouter";
 import memberRouter from "./memberRouter";
+import Main from "../pages/redux/Main";
 
 // 메인패스 컴포넌트
 const LazyMainPage = lazy(() => import("../pages/MainPage"));
@@ -18,6 +19,10 @@ const LazyFormPage = lazy(() => import("../pages/forms/FormPage"));
 const LazyMapPage = lazy(() => import("../pages/map/MapPage"));
 
 const router = createBrowserRouter([
+  {
+    path: "/rd",
+    element: <Main />,
+  },
   {
     path: "/",
     element: (
