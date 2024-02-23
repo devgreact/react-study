cbad05c49e485706e33f4e2e0770f0b1d63e9a10
devgreact <dev.greact@gmail.com>
2024-02-23 13:19:39 +0900

    타입스크립트 2 제네릭

--- src/tsbasic.ts
+++ src/tsbasic.ts
@@ -1,4 +1,15 @@
-interface Fn {
-  // 화살표 함수로 즉시 생성도 가능
-  (num: number): number;
+interface Pair<T extends number | string> {
+  first: T;
 }
+
+const pair1: Pair<number> = { first: 1 };
+// interface Pair {
+//   first: number;
+// }
+const pair2: Pair<string> = { first: "a" };
+// interface Pair {
+//   first: string;
+// }
+
+// 제약 조건에 의해 오류로 인식
+const pair3: Pair<boolean> = { first: true };
